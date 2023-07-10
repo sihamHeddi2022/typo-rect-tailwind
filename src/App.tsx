d0c7019f499dc7f2f2f9f7a6ab7e9b6069fbe626
siham <sihamheddi945@gmail.com>
2023-07-10 12:54:51 +0100

    tailwind css part

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-import Advanced from "./components/Advanced"
-import Children from "./components/Children"
-import ChildrenReact from "./components/ChildrenReact"
-import Simple from "./components/Simple"
+
 
 
 function App() {
-  const persons = [{
-    name:"djd djdj"
-  },
-  {
-    name:"keejejrejerjjerj"
-  }
-]
+
   return (
     <>
-     <Simple message="dkkddkdkkd" category="info"/>
-       <Advanced persons={persons} />
-       <Children>
-         jejdjjdsjds djdsjjsd
-       </Children>
-       <ChildrenReact>
-        <p>jsjjsjsj</p>
-       </ChildrenReact>
+      {/* <div className="flex flex-col hover:flex-row">
+         <a href="">home</a>
+         <a href="">about</a>
+         <a href="">contact</a>
+      </div> */}
+      <nav className="animate-pulse flex justify-center items-center text-xl  text-indigo-600 hover:text-fuchsia-950/90		gap-2 my-5	">
+        <a href="" className="hover:overline"> Home</a>
+        <a href="">Contact</a>
+        <a href="">About Us</a>
+        <a href="">Good Bye</a>
+      </nav>
+      <div className=" animate-pulse rounded-lg mx-5 h-14 bg-gradient-to-r from-cyan-500 to-blue-500"></div>
+      <button className="animate-pulse border bg-midnight text-tahiti rounded-full border-fuchsia-700	border-solid	">Save Changes</button>
+
     </>
   )
 }
