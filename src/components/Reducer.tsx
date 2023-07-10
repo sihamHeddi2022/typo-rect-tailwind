2ec062d8246df38f3ecf675c9080d7a41c480fd0
siham <sihamheddi945@gmail.com>
2023-07-10 11:16:57 +0100

    hooks

--- src/components/Reducer.tsx
+++ src/components/Reducer.tsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+
+type State= {
+  counter:number
+}
+
+
+const initialState:State = {counter:0}
+
+type Action = {type:"increment" | "decrement"}
+
+const reducer = (state:State,action:Action)=>{
+    switch (action.type) {
+      case "increment":
+        return {counter:state.counter+1}
+
+     case "decrement":
+                return {counter:state.counter-1}
+
+      default:
+        return {counter:state.counter}
+    }
+
+
+
+}
+
+
+
+
+
+
 
 export default function Reducer() {
+  const [state, dispatch] = useReducer(reducer,initialState)
+ 
+ 
   return (
-    <div>Reducer</div>
+    <div>
+
+
+
+       <p> counter {state.counter}</p>
+
+      <button onClick={()=> dispatch({type:"increment"})}>
+        increment
+      </button>
+
+     <button onClick={()=>dispatch({type:"decrement"})}>
+        decrement
+     </button>
+
+
+
+
+    </div>
   )
 }
